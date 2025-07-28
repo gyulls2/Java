9d36fb2454dacc3d1f49fbbf259ef3e83c6b70c8
gyull <kuri6233@gmail.com>
2025-07-29 07:58:59 +0900

    2. 변수

---
+++ src/variable/Var1.java
@@ -0,0 +1,11 @@
+package variable; // 소속된 패키지 선언
+
+public class Var1 {
+
+    public static void main(String[] args) {
+        System.out.println(20); // 10 -> 20 변경
+        System.out.println(20); // 10 -> 20 변경
+        System.out.println(20); // 10 -> 20 변경
+
+    }
+}
